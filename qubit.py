86d8b46f03122cedfc0b1fb847f8bf5749aa83aa
chtang <chtang@hmc.edu>
2024-03-07 22:12:06 -0800

    setup

--- qubit.py
+++ qubit.py
@@ -1,18 +1,32 @@
 import numpy as np
 
 class basis:
-    def __init__(self, states = np.eye(2), normalized = True):
+    '''
+    basis class
+    '''
+    def __init__(self, special: str = None, states = None, normalize = False):
         '''
         states (matrix): state in |0>, |1> basis
         '''
+        if states is None:
+            if special is not None:
+                if special == '+-':
+                    states = np.array([[1, 1], [1, -1]])/np.sqrt(2)
+                elif special == '01':
+                    states = np.array([[1, 0], [0, 1]])
+                elif special == '10':
+                    states = np.array([[0, 1], [1, 0]])
+            else:
+                states = np.eye(2)
+                
         assert len(states) == 2, "Basis must be 2D"
-        assert len(states[i] for i in range(2)) == 2, "Basis must be 2D"
+        for state in states:
+            assert len(state) == 2, "Basis must be 2D"
         assert np.isclose(np.dot(np.conj(states[0]), states[1]), 0), "Basis states must be orthogonal"
-        if normalized:
-            assert np.isclose(np.linalg.det(states), 1), "Basis states must be normalized"
+        if normalize:
+            states = np.array([states[0]/np.linalg.norm(states[0]), states[1]/np.linalg.norm(states[1])])
         else:
-            assert np.linalg.det(states) != 0, "Basis states must be linearly independent"
-            # normalize states
+            assert np.isclose(np.linalg.norm(states[0]), 1) and np.isclose(np.linalg.norm(states[1]), 1), "Basis states must be normalized"
         
         self.states = states
         self.dim = 2
@@ -24,9 +38,12 @@ class basis:
         return str(self.states)
 
 class qubit:
-    def __init__(self, state, basis = ['0', '1']):
+    '''
+    qubit class
+    '''
+    def __init__(self, state = np.array([1,0]), basis = basis()):
         norm = np.linalg.norm(state)
-        assert np,isclose(norm, 1), "State must be normalized"
+        assert np.isclose(norm, 1), "State must be normalized"
         self.state = state
         self.basis = basis
         
@@ -36,11 +53,19 @@ class qubit:
     def __repr__(self):
         return str(self.state)
     
+    def change_basis(self, new_basis):
+        '''
+        change the basis of the qubit
+        '''
+        self.state = np.dot(np.conj(np.transpose(new_basis.states)), self.state)
+        self.basis = new_basis
+    
     def probs(self, basis = None):
+        '''
+        get the probabilities of measuring |0> and |1> in the given basis
+        '''
         if basis is None:
             basis = self.basis
             
-        return np.abs(self.state)**2
-    
     def measure(self, basis = ['0', '1']):
-        
+        return 0
