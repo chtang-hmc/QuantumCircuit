a8142298b00f544dc355e29f8c2ba626551446ee
chtang <chtang@hmc.edu>
2024-03-19 14:11:53 -0700

    added node functons

--- my_graph.py
+++ my_graph.py
@@ -2,8 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import networkx as nx
 import scipy as sp
+import quantum_graph as qg
 
-class Qubits:
-    def __init__(self, n: int):
-        self.n = n
-        
+class Qubits(qg.Graph):
+    def __init__(self, size):
+        super().__init__(size)
+        for i in range(size):
+            self.add_edge(self.root, self.nodes[i+1], 0)
+            for j in range(i, size):
+                self.add_edge(self.nodes[i+1], self.nodes[j+1], 0)
+                self.add_edge(self.nodes[j+1], self.nodes[i+1], 0)
+        for node in self.nodes:
+            self.add_edge(node, node, 0)
--- quantum_graph.py
+++ quantum_graph.py
@@ -5,13 +5,28 @@ import scipy as sp
 
 class Node:
     def __init__(self):
-        self.edges = []
+        self.in_edges = []
         
     def __getitem__(self, i):
         if self.edges[i]:
             return self.edges[i].weight
         else:
             return float('inf')
+        
+    def neighbors(self):
+        return [edge.v for edge in self.edges]
+    
+    def degree(self):
+        return len(self.edges)
+    
+    def add_edge(self, v, weight):
+        self.edges.append(Edge(self, v, weight))
+    
+    def __str__(self):
+        return f'Node: {self.edges} edges'
+    
+    def __repr__(self):
+        return f'Node: {self.edges} edges'
 
 class Qubit(Node):
     def __init__(self, state = 0):
@@ -35,12 +50,27 @@ class Graph:
     def __init__(self, size):
         self.root = Root()
         self.size = size
-        self.nodes = [Qubit() for _ in range(size)] + [Qubit(1) for _ in range(size)]
+        self.nodes = [self.root] + [Qubit() for _ in range(size)] + [Qubit(1) for _ in range(size)]
         self.edges = []
+        
+    def __str__(self) -> str:
+        return f'Graph: {self.nodes} nodes and {self.edges} edges'
+    
+    def __getitem__(self, i):
+        return (self.nodes[2*i -1 ], self.nodes[2*i)
         
     def add_edge(self, u, v, weight):
         new_edge = Edge(u, v, weight)
         u.edges.append(new_edge)
         self.edges.append(new_edge)
+        
+    def delete_edge(self, u, v):
+        for edge in u.edges:
+            if edge.v == v:
+                u.edges.remove(edge)
+                self.edges.remove(edge)
+                break
+                
+    def adj
         
     
